25ecd0e30e84d68dfb5025722ca67b0b87848829
Grayson Erhard <develop@graysonerhard.com>
2026-02-24 22:57:00 -0700

    feat: update siteUrl to point to the production URL for consistency

--- lib/env.dart
+++ lib/env.dart
@@ -1,8 +1,6 @@
 class Env {
   static const String title = 'Toot Fruit';
   static const bool isProduction = bool.fromEnvironment('dart.vm.product');
-  static const prodUrl = 'https://gnarhard.dev'; // No trailing slash, please.
-  static const devUrl = 'https://gnarhard.dev'; // No trailing slash, please.
-  static const String siteUrl = isProduction ? prodUrl : devUrl;
+  static const siteUrl = 'https://gnarhard.com'; // No trailing slash, please.
   Env._();
 }
